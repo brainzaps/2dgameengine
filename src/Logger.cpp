b32af4a3226d403c40b7efb949a57043a5f90013
brainzaps <skrypak.cookiedev@gmail.com>
2024-01-30 02:10:07 +0200

    feat: save logs to vector

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -6,8 +6,11 @@
 #include <iostream>
 #include <iomanip>
 #include <chrono>
+#include <sstream>
 
 #include "Logger.h"
+
+std::vector <LogEntry> Logger::messages;
 
 void Logger::Log(const std::string &message) {
     auto now = std::chrono::system_clock::now();
@@ -16,15 +19,17 @@ void Logger::Log(const std::string &message) {
 
     std::tm *localTime = std::localtime(&currentTime);
 
+    std::ostringstream oss;
+    oss << "LOG | " << std::put_time(localTime, "%d-%b-%Y %H:%M:%S") << " - " << message;
 
-    std::cout
-            << ANSI_GREEN
-            << "LOG | "
-            << std::put_time(localTime, "%d-%b-%Y %H:%M:%S")
-            << " - "
-            << message
-            << ANSI_GREEN
-            << std::endl;
+    LogEntry entry = {
+            LogType::INFO,
+            oss.str(),
+    };
+
+    std::cout << ANSI_GREEN << oss.str() << ANSI_GREEN << std::endl;
+
+    messages.push_back(entry);
 
 }
 
@@ -36,12 +41,15 @@ void Logger::Err(const std::string &message) {
 
     std::tm *localTime = std::localtime(&currentTime);
 
-    std::cout
-            << ANSI_RED
-            << "ERR | "
-            << std::put_time(localTime, "%d-%b-%Y %H:%M:%S")
-            << " - "
-            << message
-            << ANSI_RED
-            << std::endl;
+    std::ostringstream oss;
+    oss << "ERR | " << std::put_time(localTime, "%d-%b-%Y %H:%M:%S") << " - " << message;
+
+    LogEntry entry = {
+            LogType::ERR,
+            oss.str(),
+    };
+
+    std::cout << ANSI_RED << oss.str() << ANSI_RED << std::endl;
+
+    messages.push_back(entry);
 }
--- src/Logger.h
+++ src/Logger.h
@@ -5,12 +5,26 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 const std::string ANSI_RED = "\x1B[31m";
 const std::string ANSI_GREEN = "\x1B[32m";
 
+enum LogType {
+    INFO,
+    ERR,
+    WARN,
+};
+
+struct LogEntry {
+    LogType type;
+    std::string message;
+};
+
 class Logger {
 public:
+    static std::vector <LogEntry> messages;
+
     static void Log(const std::string &message);
 
     static void Err(const std::string &message);
