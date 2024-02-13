1734c30fdb8d4a6f2e2288ff7ac614d245a878f2
brainzaps <skrypak.cookiedev@gmail.com>
2024-02-13 23:44:01 +0200

    feat: register movement system

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -13,30 +13,27 @@
 #include "../ECS/ECS.h"
 #include "../Components/TransformComponent.h"
 #include "../Components/RigidBodyComponent.h"
+#include "../Systems/MovementSystem.h"
 
 Game::Game() {
     isRunning = false;
     registry = std::make_unique<Registry>();
-    Logger::Log("Game constructor");
+    Logger::Log("Game constructor called!");
 }
 
 Game::~Game() {
-    // TODO(Skrypak): Implement
-    Logger::Log("Game destructor");
+    Logger::Log("Game destructor called!");
 }
 
 void Game::Initialize() {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-        Logger::Log("Error initializing SDL");
+        Logger::Err("Error initializing SDL.");
         return;
     }
-
     SDL_DisplayMode displayMode;
     SDL_GetCurrentDisplayMode(0, &displayMode);
-
     windowWidth = displayMode.w;
     windowHeight = displayMode.h;
-
     window = SDL_CreateWindow(
             NULL,
             SDL_WINDOWPOS_CENTERED,
@@ -45,39 +42,78 @@ void Game::Initialize() {
             windowHeight,
             SDL_WINDOW_BORDERLESS
     );
-
     if (!window) {
-        Logger::Err("Error creating SDL window");
+        Logger::Err("Error creating SDL window.");
         return;
     }
-
     renderer = SDL_CreateRenderer(window, -1, 0);
-
     if (!renderer) {
-        Logger::Err("Error creating SDL renderer");
+        Logger::Err("Error creating SDL renderer.");
         return;
     }
-
     SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
-
     isRunning = true;
-
-    Logger::Log("Game is initialized");
 }
 
+void Game::ProcessInput() {
+    SDL_Event sdlEvent;
+    while (SDL_PollEvent(&sdlEvent)) {
+        switch (sdlEvent.type) {
+            case SDL_QUIT:
+                isRunning = false;
+                break;
+            case SDL_KEYDOWN:
+                if (sdlEvent.key.keysym.sym == SDLK_ESCAPE) {
+                    isRunning = false;
+                }
+                break;
+        }
+    }
+}
 
 void Game::Setup() {
-    Logger::Log("Game setup");
+    // Add the systems that need to be processed in our game
+    registry->AddSystem<MovementSystem>();
 
+    // Create an entity
     Entity tank = registry->CreateEntity();
 
-    tank.AddComponent<TransformComponent>(glm::vec2(0, 0), glm::vec2(32, 32), 0.0);
-    tank.AddComponent<RigidBodyComponent>(glm::vec2(50, 0));
+    // Add some components to that entity
+    tank.AddComponent<TransformComponent>(glm::vec2(10.0, 30.0), glm::vec2(1.0, 1.0), 0.0);
+    tank.AddComponent<RigidBodyComponent>(glm::vec2(10.0, 50.0));
+}
+
+void Game::Update() {
+    // If we are too fast, waste some time until we reach the MILLISECS_PER_FRAME
+    int timeToWait = MILLISECS_PER_FRAME - (SDL_GetTicks() - millisecsPreviousFrame);
+    if (timeToWait > 0 && timeToWait <= MILLISECS_PER_FRAME) {
+        SDL_Delay(timeToWait);
+    }
+
+    // The difference in ticks since the last frame, converted to seconds
+    double deltaTime = (SDL_GetTicks() - millisecsPreviousFrame) / 1000.0;
+
+    // Store the "previous" frame time
+    millisecsPreviousFrame = SDL_GetTicks();
+
+    // Update the registry to process the entities that are waiting to be created/deleted
+    registry->Update();
+
+    // Ask all the systems to update
+    registry->GetSystem<MovementSystem>().Update();
+}
+
+void Game::Render() {
+    SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
+    SDL_RenderClear(renderer);
+
+    // TODO: Render game objects...
+
+    SDL_RenderPresent(renderer);
 }
 
 void Game::Run() {
     Setup();
-
     while (isRunning) {
         ProcessInput();
         Update();
@@ -85,66 +121,8 @@ void Game::Run() {
     }
 }
 
-void Game::ProcessInput() {
-    SDL_Event event;
-
-    while (SDL_PollEvent(&event)) {
-        switch (event.type) {
-            case SDL_QUIT: {
-                isRunning = false;
-                break;
-            }
-            case SDL_KEYDOWN: {
-                if (event.key.keysym.sym == SDLK_ESCAPE) {
-                    isRunning = false;
-                }
-                break;
-            }
-        }
-    }
-}
-
-void Game::Update() {
-
-    int timeToWait = MILLIS_PER_FRAME - (SDL_GetTicks() - ticksLastFrame);
-
-    if (timeToWait > 0 && timeToWait <= MILLIS_PER_FRAME) {
-        SDL_Delay(timeToWait);
-    }
-
-    double deltaTime = (SDL_GetTicks() - ticksLastFrame) / 1000.0;
-
-    ticksLastFrame = SDL_GetTicks();
-}
-
-void Game::Render() {
-
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-    SDL_RenderClear(renderer);
-
-    // Load PNG image
-    SDL_Surface *surface = IMG_Load("./assets/images/tank-tiger-right.png");
-    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
-    SDL_FreeSurface(surface);
-
-
-    // Render image
-    SDL_Rect sourceRectangle = {
-            10,
-            10,
-            32,
-            32
-    };
-    SDL_RenderCopy(renderer, texture, NULL, &sourceRectangle);
-    SDL_DestroyTexture(texture);
-
-    SDL_RenderPresent(renderer);
-}
-
 void Game::Destroy() {
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
-
-    Logger::Log("Game is destroyed");
 }
--- src/Game/Game.h
+++ src/Game/Game.h
@@ -10,30 +10,27 @@
 #include "../ECS/ECS.h"
 
 const int FPS = 60;
-const int MILLIS_PER_FRAME = 1000 / FPS;
+const int MILLISECS_PER_FRAME = 1000 / FPS;
 
 class Game {
 private:
     bool isRunning;
-    int ticksLastFrame = 0;
+    int millisecsPreviousFrame = 0;
     SDL_Window *window;
     SDL_Renderer *renderer;
 
-    std::unique_ptr<Registry> registry;
+    std::unique_ptr <Registry> registry;
 
 public:
     Game();
 
     ~Game();
 
-    int windowWidth;
-    int windowHeight;
-
     void Initialize();
 
-    void Setup();
-
     void Run();
+
+    void Setup();
 
     void ProcessInput();
 
@@ -42,5 +39,8 @@ public:
     void Render();
 
     void Destroy();
+
+    int windowWidth;
+    int windowHeight;
 };
 
